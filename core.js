18027586c158574313edeeb1246ce63cb6f151f6
Whateverayn <volpb14699@yahoo.co.jp>
2023-11-25 01:53:03 +0900

    top page

--- core.js
+++ core.js
@@ -1,7 +1,25 @@
 function generateGlobalComponents() {
     const mainroot = document.getElementById('root');
-    mainroot.insertAdjacentHTML('beforebegin',`<header><h1>${pageInfo.title}</h1><p>共通ヘッダ</p></header>`);
-    mainroot.insertAdjacentHTML('afterend',`<footer><h2>共通フッタ</h2><p>${pageInfo.description}</p></footer>`);
+
+    if (pageInfo.title === undefined) {
+        pageInfo.title = document.title;
+    }
+    if (pageInfo.description === undefined) {
+        pageInfo.description = document.querySelector("meta[name='description']").content;
+    }
+    mainroot.insertAdjacentHTML('beforebegin',`
+        <header>
+            <div id="headerTitle">
+                <a href="/">
+                    <h1>${pageInfo.title}</h1>
+                </a>
+            </div>
+        </header>`);
+    mainroot.insertAdjacentHTML('afterend',`
+        <footer>
+            <h2>共通フッタ</h2>
+            <p>${pageInfo.description}</p>
+        </footer>`);
 }
 
 generateGlobalComponents();
