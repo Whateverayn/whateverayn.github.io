a0e020da2bab67c5c7f28882a5835ffc85551c09
Whateverayn <volpb14699@yahoo.co.jp>
2024-06-04 01:05:19 +0900

    Enhance: Improve link title retrieval by using CSV data and caching

--- get_title.js
+++ get_title.js
@@ -3,6 +3,9 @@ import { launch } from "puppeteer";
 import fs from "fs";
 import path from "path";
 import { exec } from "child_process";
+import { parse } from 'url';
+import csvWriter from 'csv-writer';
+import { timeEnd } from "console";
 
 const dirPath = './docs';
 
@@ -38,7 +41,7 @@ const dirPath = './docs';
     const hrefs = [];
     const files = await searchFiles(dirPath);
 
-    spinner.succeed(`${Object.keys(files).length}件のページを検出`);
+    spinner.succeed(`${files.length}件のページを検出`);
     spinner.text = '準備しています...';
     spinner.start();
 
@@ -47,14 +50,31 @@ const dirPath = './docs';
     for (const file of files) {
         count++;
         spinner.text = `${count}`;
-        const filePath = file.dir.replace("docs\\", '');
+        const filePath = file.dir.replace("docs\\", '').replace(/\\/g, '/');
         await page.goto(`http://localhost:3000/${filePath}`);
 
-        // const autoLinks = await page.$$('a[href^="auto-"]');
-        // for (const autoLink of autoLinks) {
-        //   const href = await autoLink.evaluate(el => el.getAttribute('href'));
-        //   hrefs.push(href);
-        // }
+        // ページタイトルを取得
+        const title = await page.title();
+        // 外部リンクを取得
+        const links = await page.$$eval('a', as => as.map(a => ({ href: a.href, text: a.innerText })));
+
+        links.forEach(link => {
+            const parsedUrl = parse(link.href);
+            if (parsedUrl.host && !parsedUrl.host.includes('localhost')) {
+                hrefs.push({
+                    url: link.href,
+                    title: link.text.replace(/"/g, '""')
+                });
+                console.log(` ${link.href}: ${link.text}`);
+            }
+        });
+
+        hrefs.push({
+            url: `https://whateverayn.github.io/${filePath}`,
+            title: title.replace(/"/g, '""')
+        }); 
+        console.log(` ${filePath}: ${title}`);
+
     }
 
     child.kill();
@@ -65,6 +85,27 @@ const dirPath = './docs';
 
     spinner.succeed('完了');
 
-    process.exit(1);
+     // 重複を削除
+     const uniqueHrefs = Array.from(new Set(hrefs.map(href => href.url)))
+     .map(url => {
+         return hrefs.find(href => href.url === url);
+     });
+
+    // 名前順にソート
+    uniqueHrefs.sort((a, b) => a.title.localeCompare(b.title));
+
+    // CSVに書き出し
+    const writer = csvWriter.createObjectCsvWriter({
+        path: './docs/output.csv',
+        header: [
+            { id: 'url', title: 'URL' },
+            { id: 'title', title: 'タイトル' }
+        ],
+        alwaysQuote: true
+    });
+
+    await writer.writeRecords(uniqueHrefs);
+
+    process.exit(0);
 })();
 
